35fa24f2f6b200bff3376f49e243ddfd5575e76f
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-04-20 11:33:26 -0300

    feat: upload files via Android Dev Tools

---
+++ patches/build.gradle.kts
@@ -0,0 +1,10 @@
+patches {
+    about {
+        name = "Rhyan57 Patches"
+        description = "ReVanced patches by rhyan57 - YouTube Go features, video download and more"
+        source = "https://github.com/rhyan57/rhyan57-patches"
+        author = "rhyan57"
+        contact = "https://discord.gg/rhyan57"
+        license = "GNU General Public License v3.0"
+    }
+}
